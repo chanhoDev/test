7770ad72d76a677b9601fb182869d7ed68b1e844
chan33344 <chan33344@gmail.com>
2020-12-03 00:04:47 +0900

    test 적용 중

---
+++ src/main/java/com/local/manager/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.local.manager.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    public final String name;
+    public final int amount;
+
+}
